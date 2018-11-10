c89d1cac7d1eb8d21628f5cc78ea39970073d04a
NathanD88 <hobosanchez88@gmail.com>
2018-11-10 12:18:33 -0500

    added some more stuff to the index page, more images

--- scripts.js
+++ scripts.js
@@ -12,3 +12,44 @@ function scrollCheck() {
 function goToTop() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var sldindex = 1;
+ShowSlide(sldindex);
+
+function ChangeImage(n) {
+    ShowSlide(sldindex += n);
+}
+
+function ShowSlide (n) {
+    //get the images for the slide by class name
+    var slds = document.getElementsByClassName("slides");
+    //check to see if the next index is valid
+    if(n > slds.length)
+    {
+        sldindex = 1;
+    } else if(n < 1) {
+        sldindex = slds.length;
+    }
+    //loop through the slides and turn them off
+    for(var i = 0; i < slds.length; i++)
+    {
+        slds[i].style.display = "none";
+    }
+    //turn on the image to be shown
+    slds[sldindex-1].style.display = "block";
+    //update the text
+    NewsText();
+}
+
+function NewsText() {
+    if(sldindex == 1)
+    {
+        document.getElementById("newsheader").innerHTML = "Welcome to Car Rampage!";
+        document.getElementById("newstext").innerHTML = "Welcome to Car Rampage and thanks for checking us out. Head over to the downloads page to grab this game. Want to know more about the devs or the game? check out the about page.";
+    }
+    if(sldindex == 2)
+    {
+        document.getElementById("newsheader").innerHTML = "Tire Marks image!";
+        document.getElementById("newstext").innerHTML = "this is the text that goes along with the tire marks image. change this image with relevant news, probably have atleast 3 elements to scroll through";
+    }
+}
